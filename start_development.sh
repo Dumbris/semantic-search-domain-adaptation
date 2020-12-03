34bfe3b0d08091bc92fdb5892d9cb1f503d41f72
Algis Dumbris <a.dumbris@gmail.com>
2020-12-03 12:08:51 +0300

    Add easy starter for development

---
+++ start_development.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+##################################################################
+# Purpose: Create virtualenv dir, setup python packages
+##################################################################
+
+set -ex
+
+###Global vars declaration
+readonly HOMEDIR="${PWD}"
+readonly VIRT_ENV_DIR=${VIRT_ENV_DIR:-".pyenv"}
+
+if [ ! -d "${VIRT_ENV_DIR}" ];then
+    virtualenv -p python3 "${VIRT_ENV_DIR}"
+else
+    echo "Found ${VIRT_ENV_DIR}, skip virtualenv creation."
+fi
+
+echo "Install packages for develop..."
+
+"${HOMEDIR}/${VIRT_ENV_DIR}/bin/python" -m spacy download en_core_web_sm
+
+for pkg_dir in "search_eval" "experiment"
+do
+    cd "${HOMEDIR}/src/${pkg_dir}"
+    "${HOMEDIR}/${VIRT_ENV_DIR}/bin/python" -m pip install -e .
+    cd "${HOMEDIR}"
+done
