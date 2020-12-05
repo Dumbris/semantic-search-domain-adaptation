3165f4d64d6c3909a2914af24ba4478573d9257d
Algis <a.dumbris@gmail.com>
2020-12-05 21:39:19 +0300

    Reduce memory usage in bm25

--- src/experiment/experiment/models/bm25.py
+++ src/experiment/experiment/models/bm25.py
@@ -9,18 +9,14 @@ from experiment.models.preprocessing import Preprocess
 class BM25(base.Model):
     def __init__(self):
         self.preproc = Preprocess()
-        self.corpus = None
         self.index = None
 
     def build_index(self, docs: List[str]):
         docs = list(docs)
-        self.corpus = self.preproc.preprocess_pipe(docs)
-        #self.corpus = [' '.join(x) for x in tokenized_docs_list]
-        self.index = BM25Okapi(self.corpus)
+        self.index = BM25Okapi(self.preproc.preprocess_pipe(docs))
 
     def generate_candidates(self, query, k=10):
         tokenized_query = self.preproc.preprocess_pipe([query])[0]
         scores = self.index.get_scores(tokenized_query)
         idx = np.argsort(scores)[::-1][:k]
-        #idx = np.argpartition(scores, -1*k)[-1*k:]
         return scores[idx], idx
