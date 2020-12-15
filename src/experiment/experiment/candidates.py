2f5f05bee7e81361aa929ee051c19d85e09589c4
Algis Dumbris <a.dumbris@gmail.com>
2020-12-15 19:13:14 +0300

    Fix new_candidates generator

--- src/experiment/experiment/candidates.py
+++ src/experiment/experiment/candidates.py
@@ -10,6 +10,7 @@ def get_new_candidates(ds_test, queries_corpus, docs_corpus, vectorizer, cfg, k:
     #Init index class
     index = ann.HNSWIndex(cfg.index.ann) #TODO: remove this dependency
     logger.info("Encode docs...")
+    #vectorized_docs = vectorizer.encode(docs_corpus[ds_test.docs], show_progress_bar=True, convert_to_numpy=True)
     vectorized_docs = vectorizer.encode(docs_corpus[ds_test.docs], show_progress_bar=True, convert_to_numpy=True)
     logger.info("Indexing docs...")
     index.build(vectorized_docs)
@@ -21,7 +22,7 @@ def get_new_candidates(ds_test, queries_corpus, docs_corpus, vectorizer, cfg, k:
     logger.info("Generate candidates for reranking...")
     candidates_pairs = []
     for (score, idx), query in zip(index.generate_candidates(vectorized_queries, k), ds_test.queries_uniq):
-        for doc_id, doc_relevancy in zip(idx, score):
+        for doc_id, doc_relevancy in zip(ds_test.docs[idx], score):
             candidates_pairs.append((query, doc_id, doc_relevancy))
     
     assert len(candidates_pairs) == k*len(ds_test.queries_uniq)
