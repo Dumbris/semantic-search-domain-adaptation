7259b984a16dc320a0d91f82bed5febdbdbe0dec
Algis <a.dumbris@gmail.com>
2020-12-19 12:06:56 +0300

    Exclude msmarco

--- run_eval_02.sh
+++ run_eval_02.sh
@@ -16,5 +16,6 @@ readonly EXPRIMENT_DIR="${REPORTSDIR}/eval_encoder"
 
 source "${VIRT_ENV_DIR}/bin/activate"
 
-eval_encoder -m models.senttrans.base_model=distilroberta-base-msmarco-v2,roberta-base-nli-stsb-mean-tokens,distilbert-base-nli-stsb-quora-ranking hydra.sweep.dir="${EXPRIMENT_DIR}"
+#eval_encoder -m models.senttrans.base_model=distilroberta-base-msmarco-v2,roberta-base-nli-stsb-mean-tokens,distilbert-base-nli-stsb-quora-ranking hydra.sweep.dir="${EXPRIMENT_DIR}"
+eval_encoder -m models.senttrans.base_model=roberta-base-nli-stsb-mean-tokens,distilbert-base-nli-stsb-quora-ranking hydra.sweep.dir="${EXPRIMENT_DIR}"
 ./collect_results.sh "${EXPRIMENT_DIR}"
